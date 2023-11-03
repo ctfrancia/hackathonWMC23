56d1f41433ba75511684dae74c6634574d5a0273
Christian Francia <christian.francia@tui.com>
2023-11-03 17:43:10 +0100

    routes

--- cmd/routes.go
+++ cmd/routes.go
@@ -9,7 +9,12 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.HandlerFunc(http.MethodGet, "/v1/skins", app.getSkinsHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/skins/available", app.getSkinsHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/skins/myskins", app.getMySkinsHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/skins/buy", app.postBuySkinsHandler)
+
+	router.HandlerFunc(http.MethodPut, "/v1/skins/color", app.putSellSkinsHandler)
 
 	return router
 }
